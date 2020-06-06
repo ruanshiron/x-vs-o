e188ca7f25ef7194928ac16ca31f367809e5b9d0
nguyenvietngocquang <nguyenvietngocquang@gmail.com>
2020-06-07 00:30:18 +0700

    Sign up/in/out

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,11 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Typography, Paper, Avatar, Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import withStyles from '@material-ui/core/styles/withStyles'
+import { Link, withRouter } from 'react-router-dom'
+import firebaseConfig from '../../firebaseConfig'
 
-function Signup() {
-  return (
-    <div>
-      Signup Page
-    </div>
-  )
+const styles = theme => ({
+	main: {
+		width: 'auto',
+		display: 'block',
+		marginLeft: theme.spacing.unit * 3,
+		marginRight: theme.spacing.unit * 3,
+		[theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+			width: 400,
+			marginLeft: 'auto',
+			marginRight: 'auto',
+		},
+	},
+	paper: {
+		marginTop: theme.spacing.unit * 8,
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+	},
+	avatar: {
+		margin: theme.spacing.unit,
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%',
+		marginTop: theme.spacing.unit,
+	},
+	submit: {
+		marginTop: theme.spacing.unit * 3,
+	},
+})
+
+function Register(props) {
+	const { classes } = props
+
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	return (
+		<main className={classes.main}>
+			<Paper className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Register Account
+       			</Typography>
+				<form className={classes.form} onSubmit={e => e.preventDefault() && false }>
+					<FormControl margin="normal" required fullWidth>
+						<InputLabel htmlFor="name">Name</InputLabel>
+						<Input id="name" name="name" autoComplete="off" autoFocus value={name} onChange={e => setName(e.target.value)} />
+					</FormControl>
+					<FormControl margin="normal" required fullWidth>
+						<InputLabel htmlFor="email">Email Address</InputLabel>
+						<Input id="email" name="email" autoComplete="off" value={email} onChange={e => setEmail(e.target.value)}  />
+					</FormControl>
+					<FormControl margin="normal" required fullWidth>
+						<InputLabel htmlFor="password">Password</InputLabel>
+						<Input name="password" type="password" id="password" autoComplete="off" value={password} onChange={e => setPassword(e.target.value)}  />
+					</FormControl>
+
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						onClick={onRegister}
+						className={classes.submit}>
+						Register
+          			</Button>
+
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="secondary"
+						component={Link}
+						to="/login"
+						className={classes.submit}>
+						Go back to Login Page
+          			</Button>
+				</form>
+			</Paper>
+		</main>
+	)
+
+	async function onRegister() {
+		try {
+			await firebaseConfig.register(name, email, password)
+			props.history.replace('/dashboard')
+		} catch(error) {
+			alert(error.message)
+		}
+	}
 }
 
-export default Signup
+export default withRouter(withStyles(styles)(Register))
