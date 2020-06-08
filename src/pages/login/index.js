4074e01c64d9266a370554031661b32b64dd4310
ruanshiron <sandt26@gmail.com>
2020-06-09 00:27:40 +0700

    ~ history

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -23,7 +23,7 @@ function Login(props) {
 	const signInWithGoogle = async () => {
 		try {
 			await firebaseConfig.signInWithGoogle()
-			props.history.replace('/dashboard')
+			history.replace('/dashboard')
 		} catch (error) {
 			message.error(error.message)
 		}
