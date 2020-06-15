6a5a21fb9598c9fe5962b847294a430cece16b7d
Hinh1009 <dinhhinh10091998@gmail.com>
2020-06-15 14:39:01 +0700

    Update ranking display

--- src/pages/social/rankings/index.js
+++ src/pages/social/rankings/index.js
@@ -1,19 +1,51 @@
 import React from 'react'
 import { Card, Typography, Table } from 'antd'
+import { firestore } from '../../../firebase'
+import {transformDocs} from '../../../util'
+const data = [];
 
-const data = [
+let loadData = async function () {
+  let result = await firestore
+    .collection('users')
+    .get()
 
-];
+  let docs = result.docs
+  let users = transformDocs(docs)
 
-for (let index = 0; index < 100; index++) {
-  data.push({
-    top: index+1,
-    username: 'vinh' + index,
-    losses: 102,
-    wins: 143 + 1 * index,
-    point: 2013 + index * 10
-  })
+  let showUserData = function(){
+    for(let i = 0;i< users.length;i++){
+      data.push({
+        top: i +1,
+        username: users[i].displayName,
+        losses: users[i].stats.losses,
+        wins: users[i].stats.wins,
+      })
+    
+    }
+    }
 }
+loadData()
+
+// let showUserData = function(){
+// for(let i = 0;i< users.length;index++){
+//   data.push({
+//     top: i +1,
+//     username: users[i].displayName,
+//     losses: users[i].stats.losses,
+//     wins: users[i].stats.wins,
+//   })
+
+// }
+// }
+// for (let index = 0; index < 100; index++) {
+//   data.push({
+//     top: index + 1,
+//     username: 'vinh' + index,
+//     losses: 102,
+//     wins: 143 + 1 * index,
+//     point: 2013 + index * 10
+//   })
+// }
 const columns = [
   {
     title: 'top',
@@ -25,7 +57,13 @@ const columns = [
     title: 'username',
     dataIndex: 'username',
     key: 'username',
+<<<<<<< Updated upstream
     render: (username, record) => <Typography.Title  level={record.top > 1 ? record.top > 3 ? 4 : 3 : 2}>{username}</Typography.Title>
+=======
+    render: (username, record) => <Typography.Title level={record.top > 1 ? record.top > 3 ? 4 : 3 : 2}>{username}</Typography.Title>,
+    ellipsis: true,
+    width: 128
+>>>>>>> Stashed changes
   },
   {
     title: 'losses',
@@ -65,9 +103,13 @@ function Rankings(props) {
           }}
           onRow={(record, rowIndex) => {
             return {
-              onClick: event => {}
+              onClick: event => { }
             }
           }}
+<<<<<<< Updated upstream
+=======
+          scroll={{ x: true }}
+>>>>>>> Stashed changes
         />
       </Card>
     </div>
---
+++ src/util.js
@@ -0,0 +1,36 @@
+
+
+function checker(board, criteria, [currentX, currentY], step) {
+  let current = board[currentX][currentY], count = 0, range = []
+
+  for (let c = - criteria + 1; c < criteria; c++) range.push(c)
+
+  for (const c of range) {
+    const [i, j] = step(c)
+
+    if (board[i] !== undefined)
+      if (board[i][j] !== undefined) {
+        if (board[i][j] !== current) count = 0
+        else count++
+        if (count === criteria) return true
+      }
+  }
+
+  return false
+}
+
+function won(board, criteria, [i, j]) {
+  let r = checker(board, criteria, [i, j], (step) => [i + step, j + step]) || checker(board, criteria, [i, j], (step) => [i, j - step]) || checker(board, criteria, [i, j], (step) => [i + step, j - step]) || checker(board, criteria, [i, j], (step) => [i + step, j])
+  return r
+}
+
+function transformDocs(docs) {
+  let datas = []
+  for (let doc of docs) {
+    let data = doc.data()
+    data.id = doc.id
+    datas.push(data)
+  }
+  return datas
+}
+export { checker, won, transformDocs }
